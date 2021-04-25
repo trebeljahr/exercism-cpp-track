90f238cc81ded3518cab0dc687c169dca4bc8bd4
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-04-25 09:31:52 +0200

    Add rna-transcription solution

--- rna-transcription/rna_transcription.cpp
+++ rna-transcription/rna_transcription.cpp
@@ -1,5 +1,36 @@
 #include "rna_transcription.h"
+#include <string>
+#include <algorithm>
+#include <iostream>
 
-namespace rna_transcription {
+char convert_single_rna_letter(char letter)
+{
+    if (letter == 'A')
+        return 'U';
+    if (letter == 'G')
+        return 'C';
+    if (letter == 'T')
+        return 'A';
+    if (letter == 'C')
+        return 'G';
+    return '.';
+}
 
-}  // namespace rna_transcription
+namespace rna_transcription
+{
+    char to_rna(char letter)
+    {
+        return convert_single_rna_letter(letter);
+    }
+    string to_rna(string sequence)
+    {
+        string mappedSequence(sequence.size(), ' ');
+        int counter = 0;
+        for (auto it = sequence.begin(); it != sequence.end(); ++it, ++counter)
+        {
+            const char letter = sequence[counter];
+            mappedSequence[counter] = convert_single_rna_letter(letter);
+        }
+        return mappedSequence;
+    }
+} // namespace rna_transcription
--- rna-transcription/rna_transcription.h
+++ rna-transcription/rna_transcription.h
@@ -1,8 +1,12 @@
 #if !defined(RNA_TRANSCRIPTION_H)
 #define RNA_TRANSCRIPTION_H
+#include <string>
+using namespace std;
 
-namespace rna_transcription {
-
-}  // namespace rna_transcription
+namespace rna_transcription
+{
+    char to_rna(char letter);
+    string to_rna(string sequence);
+} // namespace rna_transcription
 
 #endif // RNA_TRANSCRIPTION_H
