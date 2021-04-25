66bc84117f8d903ba908697ceeffe1a5ce871617
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-04-25 12:42:55 +0200

    Hamming code solution

--- hamming/hamming.cpp
+++ hamming/hamming.cpp
@@ -1,5 +1,22 @@
 #include "hamming.h"
+#include <stdexcept>
+namespace hamming
+{
+    int compute(std::string a, std::string b)
+    {
+        int result = 0;
+        if (a.size() != b.size())
+        {
+            throw std::domain_error("Strings have to be same size");
+        }
+        for (size_t i = 0; i <= a.size(); i++)
+        {
+            if (a[i] != b[i])
+            {
+                result++;
+            }
+        }
+        return result;
+    }
 
-namespace hamming {
-
-}  // namespace hamming
+} // namespace hamming
--- hamming/hamming.h
+++ hamming/hamming.h
@@ -1,8 +1,10 @@
 #if !defined(HAMMING_H)
 #define HAMMING_H
+#include <string>
 
-namespace hamming {
-
-}  // namespace hamming
+namespace hamming
+{
+    int compute(std::string a, std::string b);
+} // namespace hamming
 
 #endif // HAMMING_H
