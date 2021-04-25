b34e2c9830f80710eec75e42623fef432800fa86
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-04-25 08:43:36 +0200

    Add leap test solution

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -1,5 +1,17 @@
 #include "leap.h"
 
-namespace leap {
-
-}  // namespace leap
+namespace leap
+{
+    bool is_leap_year(int year)
+    {
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0 && (year % 400 != 0))
+            {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+} // namespace leap
