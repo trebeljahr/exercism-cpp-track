9a1c45a136a06afcb8ab2311e6e6a465be5d1a46
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-05-01 17:21:30 +0200

    Solve collatz conjecture exercise

--- collatz-conjecture/collatz_conjecture.cpp
+++ collatz-conjecture/collatz_conjecture.cpp
@@ -1,5 +1,28 @@
 #include "collatz_conjecture.h"
+#include <stdexcept>
 
-namespace collatz_conjecture {
+namespace collatz_conjecture
+{
+    int steps(int num)
+    {
+        if (num <= 0)
+        {
+            throw std::domain_error("Number needs to be bigger than 0");
+        }
+        int steps = 0;
+        while (num != 1)
+        {
+            steps++;
+            if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else
+            {
+                num = num * 3 + 1;
+            }
+        }
 
-}  // namespace collatz_conjecture
+        return steps;
+    }
+} // namespace collatz_conjecture
--- collatz-conjecture/collatz_conjecture.h
+++ collatz-conjecture/collatz_conjecture.h
@@ -1,8 +1,9 @@
 #if !defined(COLLATZ_CONJECTURE_H)
 #define COLLATZ_CONJECTURE_H
 
-namespace collatz_conjecture {
-
-}  // namespace collatz_conjecture
+namespace collatz_conjecture
+{
+    int steps(int num);
+} // namespace collatz_conjecture
 
 #endif // COLLATZ_CONJECTURE_H
